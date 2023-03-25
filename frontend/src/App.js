fb20cac20d894663b88ea4bbd0b538426285a810
Chia-Yi Lin <r10922124@ntu.edu.tw>
2023-03-26 02:26:03 +0800

    Add router for MyFridge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 // import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './HomePage';
+import MyFridge from './MyFridge/myfridge';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={ <HomePage/> } />
+        <Route path='/myfridge' element={ <MyFridge/> } />
       </Routes>
     </Router>
   );
