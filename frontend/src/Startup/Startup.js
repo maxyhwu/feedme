76b2fcae6d96ca72ff14fc38cf2b68da77886fe9
wayne05IM <72805367+wayne05IM@users.noreply.github.com>
2023-03-28 14:13:56 +0800

    changing css

--- frontend/src/Startup/Startup.js
+++ frontend/src/Startup/Startup.js
@@ -6,6 +6,7 @@ const Startup = () => {
     <div className="main-container">
       {/* NavBar */}
       <div className="navbar"></div>
+
       {/* Main Frame */}
       <div className="main">
         {/* Edit Profile */}
@@ -15,21 +16,29 @@ const Startup = () => {
             <div className="setting-bar-button-text">Edit Profile</div>
             <div className="setting-bar-button-expand"></div>
           </button>
-          <div>
-            <div>Username</div>
-            <div className="login-input-bar">Username</div>
-          </div>
-          <div>
-            <div>Interested Categories</div>
-            <div className="login-input-bar">
-              Such as Taiwanese Food, Rice or Refreshing (At most five
-              categories)
+          <div className="keyin">
+            <div className="flex_container">
+              <div>Username</div>
+              <input
+                type="text"
+                className="login-input-bar"
+                defaultValue="Username"
+              />
+            </div>
+            <div className="flex_container">
+              <div>Interested Categories</div>
+              <input
+                type="text"
+                className="login-input-bar"
+                defaultValue="Such as Taiwanese Food, Rice or Refreshing (At most five
+                categories)"
+              />
             </div>
           </div>
         </div>
 
         {/* Notification Setting */}
-        <div className="feild2">
+        {/* <div className="feild2">
           <button className="setting-bar-button">
             <div className="setting-bar-button-icon"></div>
             <div className="setting-bar-button-text">Notification Setting</div>
@@ -43,25 +52,25 @@ const Startup = () => {
             <div>Ingredient expire</div>
             <div className="bell"></div>
           </div>
-        </div>
+        </div> */}
 
         {/* Fridge Setting */}
-        <div className="feild3">
+        {/* <div className="feild3">
           <button className="setting-bar-button">
             <div className="setting-bar-button-icon"></div>
-            <div className="setting-bar-button-text">Notification Setting</div>
+            <div className="setting-bar-button-text">Fridge Setting</div>
             <div className="setting-bar-button-expand"></div>
           </button>
           <div>
             <div>Update your fridge</div>
-            <div className="login-input-bar">
+            <input type="text" className="login-input-bar">
               What’s in your fridge? (e.g. 1 carrot, 3 eggs ......)
-            </div>
+            </input>
           </div>
-        </div>
+        </div> */}
 
         {/* Submit */}
-        <div className="submit-button">Submit</div>
+        <button className="submit-button">Submit</button>
       </div>
       {/* Footer */}
       <div className="footer"></div>
