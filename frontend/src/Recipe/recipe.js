362595ee80f25352f75c2761c14a59c4fae6edd6
zhiyuuuu <liuzy0815@gmail.com>
2023-03-29 15:11:50 +0800

    Add arrows

--- frontend/src/Recipe/recipe.js
+++ frontend/src/Recipe/recipe.js
@@ -1,4 +1,5 @@
 import "./recipe.css"
+import { IoIosArrowForward } from 'react-icons/io';
 
 const Recipe = () => {
     return (
@@ -12,19 +13,21 @@ const Recipe = () => {
                         <img src="https://sudachirecipes.com/wp-content/uploads/2022/08/beef-curry-rice-thumbnail.jpg" alt="" />
                     </div>
                     <div className="poptitle">Curry Rice</div>
+                    <IoIosArrowForward/>
                 </div>
                 <div className="popRecipe">
                     <div className="popImg">
                         <img src="https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Easy-Fresh-Strawberry-Pie_EXPS_TMBBP19_34179_B06_20_3b.jpg" alt="" />
                     </div>
                     <div className="poptitle">Strawberry Pie</div>
+                    <IoIosArrowForward/>
                 </div>
                 <div className="popRecipe">
                     <div className="popImg">
                         <img src="https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/08/302612-1-eng-GB_thyme-roasted-rib-of-beef.jpg" alt="" />
                     </div>
                     <div className="poptitle">Rib</div>
-                    
+                    <IoIosArrowForward/>
                 </div>
             </div>
         </div>
