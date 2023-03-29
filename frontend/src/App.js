7d620e390f7ede9ee10d184cf51403be194ae4fc
b09705010 <b09705010@ntu.edu.tw>
2023-03-29 23:24:18 +0800

    Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,29 @@ import HomePage from './HomePage/HomePage';
 import MyFridge from './MyFridge/myfridge';
 import Recipe from './Recipe/recipe';
 import LoginPage from './Login/LoginPage';
+import Header from "./Header/Header";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={ <HomePage/> } />
-        <Route path='/myfridge' element={ <MyFridge/> } />
-        <Route path='/recipe' element={ <Recipe/> } />
-        <Route path='/login' element={ <LoginPage/> } />
-      </Routes>
-    </Router>
+    <Wrapper>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path='/' element={ <HomePage/> } />
+          <Route path='/myfridge' element={ <MyFridge/> } />
+          <Route path='/recipe' element={ <Recipe/> } />
+          <Route path='/login' element={ <LoginPage/> } />
+        </Routes>
+      </Router>
+    </ Wrapper>
   );
 }
 
