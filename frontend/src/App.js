5d881fc64407c4e8d14ac655c943ae3cdfe6bc45
joannawei <b09705023@ntu.edu.tw>
2023-03-29 14:25:32 +0800

    add recipe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './HomePage/HomePage';
 import MyFridge from './MyFridge/myfridge';
+import Recipe from './Recipe/recipe';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <HomePage/> } />
         <Route path='/myfridge' element={ <MyFridge/> } />
+        <Route path='/recipe' element={ <Recipe/> } />
       </Routes>
     </Router>
   );
