de8bc60166aaa6f4f2a64e5605b04592bb91d60e
ansonWang0125 <b09705044@ntu.edu.tw>
2023-03-29 23:36:44 +0800

    add sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './HomePage/HomePage';
 import MyFridge from './MyFridge/myfridge';
 import LoginPage from './Login/LoginPage';
+import SignupPage from './Signup/SignupPage';
+import SetPassword from './SetPassword/SetPassword';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path='/' element={ <HomePage/> } />
         <Route path='/myfridge' element={ <MyFridge/> } />
         <Route path='/login' element={ <LoginPage/> } />
+        <Route path='/signup' element={ <SignupPage/> } />
+        <Route path='/setpassword' element={ <SetPassword/> } />
       </Routes>
     </Router>
   );
---
+++ frontend/src/SetPassword/SetPassword.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './SetPassword.css';
+import FeedMe from '../assets/FeedMe.jpg';
+
+export default function SetLogin () {
+
+
+
+    return (
+        <div className="wrapper">
+            <div className="body">
+                <div id='logo'>
+                    <img src={FeedMe} alt='feedme' id="feedmelogo"/>
+                </div>
+                <form id="info">
+                <div className="infos" id="title">Reset Password</div>
+                    <input type="text" placeholder='Phone Number' className="input infos" autoComplete='off'/>
+                    <div className="d-flex" >
+                        <input type="text" placeholder='Verify Code' className="input infos mr-auto p-2" id="code" autoComplete='off'/>
+                        <button id="send" className="p-2" type="button">Get Verification Code</button>
+                    </div>
+                    <input type="password" placeholder='New Password' className="input infos" autoComplete='off'/>
+                    <input type="submit" value="Reset" className="infos" id="reset"/>
+                </form>
+            </div>
+        </div>
+    )
+}
