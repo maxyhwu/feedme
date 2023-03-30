121823853738401aaa896bc9c2d1b9d92f2141f5
ansonWang0125 <b09705044@ntu.edu.tw>
2023-03-31 01:36:06 +0800

    test language switch

--- frontend/src/Login/LoginPage.js
+++ frontend/src/Login/LoginPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './LoginPage.css';
 import FeedMe from '../assets/FeedMe.jpg';
 import twitterLogo from '../assets/twitterLogo.png';
@@ -10,6 +10,7 @@ import {
 import { ReactComponent as GoogleLogo } from '../assets/google.svg';
 import { getGoogleUrl } from '../utils/getGoogleUrl';
 import { onSuccess, onFailed } from '../utils/getTwittertoken';
+import { FormattedMessage, IntlProvider } from "react-intl";
 
 export default function LoginPage () {
 
@@ -20,30 +21,65 @@ export default function LoginPage () {
     const location = useLocation();
     const [checkbox, setCheckbox] = useState(false);
     let from = ((location.state)?.from?.pathname) || '/';
+    const [lang, setLang] = useState('en')
+    const [locale, setLocale] = useState(undefined)
+
+    useEffect(() => {
+        async function fetchLang () {
+            try {
+                const resp = await fetch(`./lang/${lang}.json`)
+                const data = await resp.json()
+                console.log(data)
+                setLocale(data)
+              } catch (error) {
+                console.error(`Error fetching translation file for ${lang}:`, error)
+              }
+        }
+        fetchLang()
+      },[lang])
 
     const handleClickCheckbox = () => {
         setCheckbox(!checkbox)
     }
 
     return (
+        <IntlProvider locale="en" messages={locale}>
         <div className="container">
             <div className="body">
                 <div id='logo'>
                     <img src={FeedMe} alt='feedme' id="feedmelogo"/>
                 </div>
                 <form id="info">
-                    <div className="infos" id="welcome">Welcome</div>
-                    <input type="text" placeholder='Email ID' className="input infos" autoComplete={checkbox?'email':'off'}/>
-                    <input type="password" placeholder='Password' className="input infos" autoComplete={checkbox?'current-password':'off'}/>
+                    <div id="header">
+                        <div className="infos" id="welcome"><FormattedMessage id="login.welcome" defaultMessage="Welcome" /></div>
+                        <select
+                            value={lang}
+                            onChange={(evt) => {
+                                setLang(evt.target.value);
+                            }}
+                            id="selecter"
+                            >
+                            <option value="en">English</option>
+                            <option value="cn">中文</option>
+                        </select>
+                    </div>
+                    <FormattedMessage id="login.email" defaultMessage="Email ID" >
+                        {(msg) => (<input type="text" placeholder={msg} className="input infos" autoComplete={checkbox?'email':'off'}/>)}
+                    </FormattedMessage>
+                    <FormattedMessage id="login.password" defaultMessage="Password" >
+                        {(msg) => (<input type="password" placeholder={msg} className="input infos" autoComplete={checkbox?'current-password':'off'}/>)}
+                    </FormattedMessage>
                     <div id="passwordCtrl" className="infos">
                         <input type="checkbox" id="checkbox" className="checkbox-round" checked={checkbox} onChange={handleClickCheckbox}/>
-                        <label htmlFor="remember" onClick={handleClickCheckbox}>Remember me</label>
-                        <Link to="/setpassword" id="setpsw">Forget password?</Link>
+                        <label htmlFor="remember" onClick={handleClickCheckbox}><FormattedMessage id="login.rememberMe" defaultMessage="Remember me" /></label>
+                        <Link to="/setpassword" id="setpsw"><FormattedMessage id="login.forget" defaultMessage="forget password" />?</Link>
                     </div>
-                    <input type="submit" value="Login" className="infos" id="login"/>
+                    <FormattedMessage id="login.login" defaultMessage="Log in" >
+                        {(msg) => (<input type="submit" value={msg} className="infos" id="login"/>)}
+                    </FormattedMessage>
                     <div id='dashLine' className="infos">
                         <div id="line"></div>
-                        <div id="text">or</div>
+                        <div id="text"><FormattedMessage id="login.or" defaultMessage="or" /></div>
                         <div id="line"></div>
                     </div>
                     <div id="external" className="infos">
@@ -64,5 +100,6 @@ export default function LoginPage () {
                 </form>
             </div>
         </div>
+        </IntlProvider>
     )
 }
