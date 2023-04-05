ab599d528a9be2b9684477da0ea458224ac0b434
zhiyuuuu <liuzy0815@gmail.com>
2023-04-05 14:48:23 +0800

    add navigate to elements

--- frontend/src/HomePage/HomePage.js
+++ frontend/src/HomePage/HomePage.js
@@ -102,7 +102,7 @@ const HomePage = () => {
                                 </div>
                             </div>
                             <div className="learnmore">
-                                <button className="button-learnmore">Learn More</button>
+                                <button className="button-learnmore" onClick={navigateToDetail}>Learn More</button>
                             </div>
                         </div>
                     </div>
@@ -112,21 +112,21 @@ const HomePage = () => {
                         Our Popular Recipes
                     </div>
                     <div className="popRecipes-container">
-                        <div className="popRecipe">
+                        <div className="popRecipe" onClick={navigateToDetail}>
                             <div className="popImg">
                                 <img src="https://sudachirecipes.com/wp-content/uploads/2022/08/beef-curry-rice-thumbnail.jpg" alt="" />
                             </div>
                             <div className="poptitle">Curry Rice</div>
                             <IoIosArrowForward/>
                         </div>
-                        <div className="popRecipe">
+                        <div className="popRecipe" onClick={navigateToDetail}>
                             <div className="popImg">
                                 <img src="https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Easy-Fresh-Strawberry-Pie_EXPS_TMBBP19_34179_B06_20_3b.jpg" alt="" />
                             </div>
                             <div className="poptitle">Strawberry Pie</div>
                             <IoIosArrowForward/>
                         </div>
-                        <div className="popRecipe">
+                        <div className="popRecipe" onClick={navigateToDetail}>
                             <div className="popImg">
                                 <img src="https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/08/302612-1-eng-GB_thyme-roasted-rib-of-beef.jpg" alt="" />
                             </div>
