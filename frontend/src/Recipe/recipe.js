6c53d31e2cd5f77991853d5c6fbb6ea5f177f1c0
zhiyuuuu <liuzy0815@gmail.com>
2023-04-07 16:52:59 +0800

    add navigation to <Recipe/>

--- frontend/src/Recipe/recipe.js
+++ frontend/src/Recipe/recipe.js
@@ -1,8 +1,15 @@
 import "./recipe.css"
 import { IoIosArrowForward } from 'react-icons/io';
 import SearchBar from "./searchbar";
+import { useNavigate } from "react-router-dom";
 
 const Recipe = () => {
+
+    const navigate = useNavigate();
+    const navigateToDetail = () => {
+        navigate('/modal');
+    }
+
     return (
         <div className="recipe-container">
             <SearchBar />
@@ -11,21 +18,21 @@ const Recipe = () => {
                     Our Popular Recipes
                 </div>
                 <div className="popRecipes-container">
-                    <div className="popRecipe">
+                    <div className="popRecipe" onClick={navigateToDetail}>
                         <div className="popImg">
                             <img src="https://sudachirecipes.com/wp-content/uploads/2022/08/beef-curry-rice-thumbnail.jpg" alt="" />
                         </div>
                         <div className="poptitle">Curry Rice</div>
                         <IoIosArrowForward/>
                     </div>
-                    <div className="popRecipe">
+                    <div className="popRecipe" onClick={navigateToDetail}>
                         <div className="popImg">
                             <img src="https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Easy-Fresh-Strawberry-Pie_EXPS_TMBBP19_34179_B06_20_3b.jpg" alt="" />
                         </div>
                         <div className="poptitle">Strawberry Pie</div>
                         <IoIosArrowForward/>
                     </div>
-                    <div className="popRecipe">
+                    <div className="popRecipe" onClick={navigateToDetail}>
                         <div className="popImg">
                             <img src="https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/08/302612-1-eng-GB_thyme-roasted-rib-of-beef.jpg" alt="" />
                         </div>
