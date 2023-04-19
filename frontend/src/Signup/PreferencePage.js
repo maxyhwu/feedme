38110bb25e9315100df99aaab5002c0e4f6735bf
zhiyuuuu <liuzy0815@gmail.com>
2023-04-19 11:37:58 +0800

    update functions & limitation

--- frontend/src/Signup/PreferencePage.js
+++ frontend/src/Signup/PreferencePage.js
@@ -1,8 +1,38 @@
 import "./PreferencePage.css";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Preference = () => {
 
     const categories = ['Chinese', 'Japanese', 'American', 'French', 'Korean', 'Italian', 'Thai', 'Vietnam', 'Taiwanese']
+
+    const navigate = useNavigate();
+    const navigateToHome = () => {
+        navigate('/');
+    }
+
+    const [checked, setChecked] = useState(0);
+    const [uponLimit, setUponLimit] = useState(false);
+
+    const handleChecked = (e) => {
+        const isChecked = e.target.checked;
+        if (isChecked) {
+            setChecked(checked + 1);
+        } else {
+            setChecked(checked - 1);
+        }
+    }
+
+    useEffect(() => {
+        function checkedLimit() {
+            if (checked > 3) {
+                setUponLimit(true);
+            } else {
+                setUponLimit(false);
+            }
+        }
+        checkedLimit();
+    }, [checked])
 
     return(
         <div className="prefer-container">
@@ -20,7 +50,7 @@ const Preference = () => {
                         categories.map((category, i) => {
                         return (<div className="genre" key={i}>
                             <div className="checkbox">
-                                <input type="checkbox" id={i} name="check"/>
+                                <input type="checkbox" id={i} name="check" onChange={handleChecked}/>
                                 <label for={i}>{ category }</label>
                             </div>
                         </div>
@@ -28,11 +58,22 @@ const Preference = () => {
                     }
                 </div>
                 <div className="prefer-bottom">
+                    <div className="error-hint" style={ uponLimit? { display: "flex"}:{ display: "none" }}>
+                        3 categories limited !
+                    </div>
                     <div className="confirm">
-                        <button className="confirm-btn">Confirm</button>
+                        <button className="confirm-btn" 
+                            style={ uponLimit? { opacity:0.5 }:{} } 
+                            disabled={ uponLimit? true:false }
+                            onClick={() => {
+                                navigateToHome();
+                                alert('Success!')
+                            }}>
+                            Confirm
+                        </button>
                     </div>
                     <div className="skip">
-                        <div className="skip-txt">
+                        <div className="skip-txt" onClick={() => navigateToHome()}>
                             Skip
                         </div>
                     </div>
