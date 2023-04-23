0a2c2357b08b931ab30de63434aecfa4ad237313
Chia-Yi Lin <r10922124@ntu.edu.tw>
2023-04-23 15:20:00 +0800

    Add sub-links to Recipes, remove Forum and star icon. Allow navbar RWD.

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -7,6 +7,11 @@ import { UseLoginContext } from "../Context/LoginCnt";
 import { UseLangContext } from "../Context/LangCnt";
 import AccountMenu from "../Components/menu";
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 const Header = () => {
     const navigate = useNavigate();
     const {lang,changeLang} = UseLangContext()
@@ -22,46 +27,79 @@ const Header = () => {
     }
 
     return (
-        <div className="navbar">
-            <div className="logoWrapper">
-                <img src={FeedMe} alt='feedme' id="feedmelogo" onClick={() => {navigate('/')}}/>
-            </div>
-            <div className="navbuttonList">
-                <button className="navbutton" onClick={() => {navigate('/')}}>
-                    Home
-                </button>
-                <button className="navbutton" onClick={() => {navigate('/recipe')}}>
-                    Recipes
-                </button>
-                <button className="navbutton" onClick={() => {}}>
-                    Forum
-                </button>
-                <button className="navbutton" onClick={() => {navigate('/myfridge')}}>
-                    My Fridge
-                </button>
-            </div>
-            <div>
-                <select
-                    value={lang}
-                    onChange={(evt) => {
-                        handleSelectChange(evt);
-                    }}
-                    id="selecter"
-                    >
-                    <option value="en">English</option>
-                    <option value="cn">中文</option>
-                </select>
-            </div>
-            <div className="nav-user">
-                <div className="nav-fav">
-                    <AiOutlineStar id="favIcon"/>
-                    <AiFillStar id="favIconHover" onClick={() => {navigate('myArchive')}}/>
+        <Navbar expand="lg">
+            <Container>
+                {/* LOGO */}
+                <Navbar.Brand>
+                    <div className="logoWrapper">
+                        <img src={FeedMe} alt='feedme' id="feedmelogo" onClick={() => {navigate('/')}}/>
+                    </div>
+                </Navbar.Brand>
+
+                {/* Language selector and user icon when screen is xs/sm/md */}
+                <div className="ms-auto d-flex d-lg-none">
+                    <div>
+                        <select
+                            value={lang}
+                            onChange={(evt) => {
+                                handleSelectChange(evt);
+                            }}
+                            id="selecter"
+                        >
+                            <option value="en">English</option>
+                            <option value="cn">中文</option>
+                        </select>
+                    </div>
+
+                    <div className="nav-user">
+                        <div className="nav-user-icon">
+                            {login ? <AccountMenu/> : <BiUserCircle id="userIcon" onClick={() => {handleUserIcon()}}/>}
+                        </div>
+                    </div>
                 </div>
-                <div className="nav-user-icon">
-                    {login? <AccountMenu/>:<BiUserCircle id="userIcon" onClick={() => {handleUserIcon()}}/>}
-                </div>
-            </div>
-        </div>
+
+                {/* Use toggle list when screen is xs/sm/md */}
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                {/* Navbar content */}
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto navbuttonList">
+                        <Nav.Link className="navbutton" onClick={() => {navigate('/')}}>Home</Nav.Link>
+
+                        <NavDropdown title="Recipes" id="basic-nav-dropdown">
+                            <NavDropdown.Item onClick={() => {navigate('/recipe')}}>All Recipes</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => {navigate('/recipe')}}>Suggest For You</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => {navigate('/recipe')}}>My Recipes</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => {navigate('/myArchive')}}>Saved Recipes</NavDropdown.Item>
+                        </NavDropdown>
+
+                        <Nav.Link onClick={() => {navigate('/myfridge')}}>My Fridge</Nav.Link>
+
+                        {/* Language selector and user icon when expanded */}
+                        <div className="ms-auto d-none d-lg-flex">
+                            <div>
+                                <select
+                                    value={lang}
+                                    onChange={(evt) => {
+                                        handleSelectChange(evt);
+                                    }}
+                                    id="selecter"
+                                >
+                                    <option value="en">English</option>
+                                    <option value="cn">中文</option>
+                                </select>
+                            </div>
+
+                            <div className="nav-user">
+                                <div className="nav-user-icon">
+                                    {login ? <AccountMenu/> : <BiUserCircle id="userIcon" onClick={() => {handleUserIcon()}}/>}
+                                </div>
+                            </div>
+                        </div>
+                    </Nav>                    
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
