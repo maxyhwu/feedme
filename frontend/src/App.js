eb45377ac8bc95b752e94d475047562de24ef127
wayneliu <72805367+wayne05IM@users.noreply.github.com>
2023-04-26 15:44:35 +0800

    close footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
             <Routes>
               <Route path='/*' element={ <Main/> } />
             </Routes>
-            <Footer/>
+            {/* <Footer/> */}
           </Router>
         </ Wrapper>
       </LoginContextProvider>
