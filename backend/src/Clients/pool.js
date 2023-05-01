25054c12a8fbc8c340a288bc90afa293b0bbc0a6
ansonWang0125 <b09705044@ntu.edu.tw>
2023-05-01 15:46:54 +0800

    test pool

---
+++ backend/src/Clients/pool.js
@@ -0,0 +1,20 @@
+import { Pool } from "pg";
+// require('dotenv').config()
+import dotenv from "dotenv-defaults";
+dotenv.config();
+
+var conString = process.env.url;
+
+const pool = new Pool({
+  max: 10,
+  // user: process.env.user,
+  // host: process.env.host,
+  // database: process.env.databaseName,
+  // password: process.env.password,
+  // port: 5432, // The default port for PostgreSQL is 5432
+  connectionString: conString
+});
+
+export {
+  pool
+};
