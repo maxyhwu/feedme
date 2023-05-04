a6deba11561920f04b7bc08dd647c3bab9ddeb8a
Chia-Yi Lin <r10922124@ntu.edu.tw>
2023-05-04 20:38:07 +0800

    recommend recipe link modal

--- frontend/src/HomePage/HomePage.js
+++ frontend/src/HomePage/HomePage.js
@@ -3,6 +3,9 @@ import "./HomePage.css"
 import { FaHeart } from 'react-icons/fa';
 import { IoIosArrowForward } from 'react-icons/io'
 import { useNavigate } from "react-router-dom";
+import Modal from 'react-modal';
+import { recipe_data } from "../Recipe/recipedata";
+import { RecipeDetail } from '../Recipe/detail';
 
 const HomePage = () => {
 
@@ -29,6 +32,115 @@ const HomePage = () => {
         // console.log('heart clicked.');
     }
 
+    const RecommendRecipeBlock = ({ recipe }) => {
+        const { recipeName, ingredients, instructions } = recipe;
+        const customModalStyles = {
+            content: {
+                width: '80%',
+                transform: 'translate(10%, 0%)', // Translate the modal to the center of the screen
+                boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.15)',
+                borderRadius: '15px',
+            }
+        };
+        const [showModal, setShowModal] = useState(false);
+    
+        const handleOpenModal = () => {
+            setShowModal(true);
+        };
+    
+        const handleCloseModal = () => {
+            setShowModal(false);
+        };
+
+        const [activeHeart, setActiveHeart] = useState(false);
+        const toggleHeart = () => {
+            setActiveHeart(!activeHeart);
+        }
+
+        return (
+            <div className="recipe">
+                <div className="recipe-header">
+                    <div className="recipe-title"> {recipeName} </div>
+                    <FaHeart style={{ color: activeHeart ? 'red' : '' }} onClick={toggleHeart}/>
+                </div>
+                <div className="view-block">
+                    
+                    <div className="recipe-content">
+                        Ingredients:<br/><br/>
+
+                        {ingredients.map((ingredient) => (
+                            <>{ingredient[0]}: {ingredient[1]}<br/></>
+                        ))}
+                        
+                        <br/>
+                        Instructions:<br/>
+                        <ol className="instructions">
+                            {instructions.map((instruction) => (
+                                <li>{instruction}</li>
+                            ))}
+                        </ol>
+                    </div>
+                </div>
+                
+                <div className="learnmore">
+                    <button className="button-learnmore" onClick={handleOpenModal}>Learn More</button>
+                    <Modal
+                        isOpen={showModal}
+                        onRequestClose={handleCloseModal}
+                        style={customModalStyles}
+                    >
+                        <RecipeDetail recipe={recipe} handleCloseModal={handleCloseModal} />
+                    
+                    </Modal>
+                </div>
+            </div>
+        )
+    }
+
+    const PopularRecipeBlock = ({ recipe }) => {
+        const { recipeName, image_link } = recipe;
+        const customModalStyles = {
+            content: {
+                width: '80%',
+                transform: 'translate(10%, 0%)', // Translate the modal to the center of the screen
+                boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.15)',
+                borderRadius: '15px',
+            }
+        };
+        const [showModal, setShowModal] = useState(false);
+    
+        const handleOpenModal = () => {
+            setShowModal(true);
+        };
+    
+        const handleCloseModal = () => {
+            setShowModal(false);
+        };
+
+        return (
+            <>
+                <div className="popRecipe" onClick={handleOpenModal}>
+                    <div className="popImg">
+                        <img src={image_link} alt="" />
+                    </div>
+                    <div className="poptitle"> {recipeName} </div>
+                    <IoIosArrowForward/>
+                </div>
+                <Modal
+                    isOpen={showModal}
+                    onRequestClose={handleCloseModal}
+                    style={customModalStyles}
+                >
+                    <RecipeDetail recipe={recipe} handleCloseModal={handleCloseModal} />
+                
+                </Modal>
+            </>
+        )
+    }
+    
+    const recommendData = recipe_data.slice(3, 5);
+    const popularData = recipe_data.slice(0, 3);
+
     return (
         <div className="main-container">
             <div className="main-homepage">
@@ -46,7 +158,11 @@ const HomePage = () => {
                         Recommended Recipes
                     </div>
                     <div className="recipes-container">
-                        <div className="recipe">
+                        {recommendData.map((recipe, index) => (
+                            <RecommendRecipeBlock key={index} recipe={ recipe } />
+                        ))}
+
+                        {/* <div className="recipe">
                             <div className="recipe-header">
                                 <div className="recipe-title"> Curry Rice </div>
                                 <FaHeart style={{ color: activeHeart ? 'red' : '' }} onClick={toggle}/>
@@ -118,7 +234,7 @@ const HomePage = () => {
                             <div className="learnmore">
                                 <button className="button-learnmore" onClick={navigateToDetail}>Learn More</button>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <div className="bottom">
@@ -126,7 +242,10 @@ const HomePage = () => {
                         Our Popular Recipes
                     </div>
                     <div className="popRecipes-container">
-                        <div className="popRecipe" onClick={navigateToDetail}>
+                        {popularData.map((recipe, index) => (
+                            <PopularRecipeBlock key={index} recipe={ recipe } />
+                        ))}
+                        {/* <div className="popRecipe" onClick={navigateToDetail}>
                             <div className="popImg">
                                 <img src="https://sudachirecipes.com/wp-content/uploads/2022/08/beef-curry-rice-thumbnail.jpg" alt="" />
                             </div>
@@ -146,7 +265,7 @@ const HomePage = () => {
                             </div>
                             <div className="poptitle">Rib</div>
                             <IoIosArrowForward/>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
