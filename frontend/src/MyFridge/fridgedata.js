1a4c9fda8178aafed5a38d40de107be8e6df3903
Chia-Yi Lin <r10922124@ntu.edu.tw>
2023-05-06 16:36:15 +0800

    hide some data

--- frontend/src/MyFridge/fridgedata.js
+++ frontend/src/MyFridge/fridgedata.js
@@ -5,17 +5,17 @@ const template_data = [
 ]
 
 const raw_data = [
-    { userIngredientID: 0, name: 'Carrots', quantity: 200, category: 'Vegetables', purchaseDate: '2023-04-15', expirationDate: '2023-04-30' },
+    // { userIngredientID: 0, name: 'Carrots', quantity: 200, category: 'Vegetables', purchaseDate: '2023-04-15', expirationDate: '2023-04-30' },
     { userIngredientID: 1, name: 'Milk', quantity: 100, category: 'Dairy', purchaseDate: '2023-04-12', expirationDate: '2023-04-16' },
     { userIngredientID: 2, name: 'Beef', quantity: 100, category: 'Meat and Poultry', purchaseDate: '2023-04-10', expirationDate: '2023-04-15' },
-    { userIngredientID: 3, name: 'Salmon', quantity: 200, category: 'Seafood', purchaseDate: '2023-04-13', expirationDate: '2023-04-28' },
-    { userIngredientID: 4, name: 'Brown rice', quantity: 30, category: 'Grains', purchaseDate: '2023-04-11', expirationDate: '2023-04-31' },
+    // { userIngredientID: 3, name: 'Salmon', quantity: 200, category: 'Seafood', purchaseDate: '2023-04-13', expirationDate: '2023-04-28' },
+    // { userIngredientID: 4, name: 'Brown rice', quantity: 30, category: 'Grains', purchaseDate: '2023-04-11', expirationDate: '2023-04-31' },
     { userIngredientID: 5, name: 'Apples', quantity: 50, category: 'Fruits', purchaseDate: '2023-04-14', expirationDate: '2023-04-20' },
     { userIngredientID: 6, name: 'Almonds', quantity: 100, category: 'Nuts and Seeds', purchaseDate: '2023-04-17', expirationDate: '2023-04-27' },
-    { userIngredientID: 7, name: 'Broccoli', quantity: 100, category: 'Vegetables', purchaseDate: '2023-04-16', expirationDate: '2023-04-20' },
+    // { userIngredientID: 7, name: 'Broccoli', quantity: 100, category: 'Vegetables', purchaseDate: '2023-04-16', expirationDate: '2023-04-20' },
     { userIngredientID: 8, name: 'Chicken', quantity: 100, category: 'Meat and Poultry', purchaseDate: '2023-04-12', expirationDate: '2023-04-23' },
-    { userIngredientID: 9, name: 'Yogurt', quantity: 20, category: 'Dairy', purchaseDate: '2023-04-09', expirationDate: '2023-04-25' },
-    { userIngredientID: 10, name: 'Tilapia', quantity: 200, category: 'Seafood', purchaseDate: '2023-04-14', expirationDate: '2023-04-18' },
+    // { userIngredientID: 9, name: 'Yogurt', quantity: 20, category: 'Dairy', purchaseDate: '2023-04-09', expirationDate: '2023-04-25' },
+    // { userIngredientID: 10, name: 'Tilapia', quantity: 200, category: 'Seafood', purchaseDate: '2023-04-14', expirationDate: '2023-04-18' },
     { userIngredientID: 11, name: 'Onions', quantity: 100, category: 'Vegetables', purchaseDate: '2023-04-17', expirationDate: '2023-04-23' },
     { userIngredientID: 12, name: 'Almonds', quantity: 100, category: 'Nuts and Seeds', purchaseDate: '2023-04-12', expirationDate: '2023-04-15' },
     { userIngredientID: 13, name: 'Pork', quantity: 200, category: 'Meat and Poultry', purchaseDate: '2023-04-16', expirationDate: '2023-04-24' },
@@ -30,12 +30,12 @@ const raw_data = [
     { userIngredientID: 22, name: 'Chicken', quantity: 300, category: 'Meat and Poultry', purchaseDate: '2023-04-10', expirationDate: '2023-04-16' },
     { userIngredientID: 23, name: 'Lettuce', quantity: 100, category: 'Vegetables', purchaseDate: '2023-04-04', expirationDate: '2023-04-11' },
     { userIngredientID: 24, name: 'Bananas', quantity: 50, category: 'Fruits', purchaseDate: '2023-04-09', expirationDate: '2023-04-14' },
-    { userIngredientID: 25, name: 'Salmon', quantity: 100, category: 'Seafood', purchaseDate: '2023-04-08', expirationDate: '2023-04-13' },
+    // { userIngredientID: 25, name: 'Salmon', quantity: 100, category: 'Seafood', purchaseDate: '2023-04-08', expirationDate: '2023-04-13' },
     { userIngredientID: 26, name: 'White Rice', quantity: 100, category: 'Grains', purchaseDate: '2023-04-06', expirationDate: '2023-04-16' },
     { userIngredientID: 27, name: 'Grapes', quantity: 50, category: 'Fruits', purchaseDate: '2023-04-13', expirationDate: '2023-04-22' },
     { userIngredientID: 28, name: 'Cucumber', quantity: 50, category: 'Vegetables', purchaseDate: '2023-04-01', expirationDate: '2023-04-08' },
-    { userIngredientID: 29, name: 'Cheddar Cheese', quantity: 50, category: 'Dairy', purchaseDate: '2023-04-05', expirationDate: '2023-04-15' },
-    { userIngredientID: 30, name: 'Quinoa', quantity: 50, category: 'Grains', purchaseDate: '2023-04-07', expirationDate: '2023-04-10' },
+    // { userIngredientID: 29, name: 'Cheddar Cheese', quantity: 50, category: 'Dairy', purchaseDate: '2023-04-05', expirationDate: '2023-04-15' },
+    // { userIngredientID: 30, name: 'Quinoa', quantity: 50, category: 'Grains', purchaseDate: '2023-04-07', expirationDate: '2023-04-10' },
 ]
 
 export const data = raw_data.reduce((accumulator, currentIngredient) => {
