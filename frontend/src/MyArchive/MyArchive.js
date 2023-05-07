55c46125a43e381d5c95b6dbbea6d95822dd0800
ansonWang0125 <b09705044@ntu.edu.tw>
2023-05-07 18:24:25 +0800

    twitter login fix

--- frontend/src/MyArchive/MyArchive.js
+++ frontend/src/MyArchive/MyArchive.js
@@ -1,16 +1,15 @@
 import './MyArchive.css';
-import { useState } from "react";
 
 import { UseDataContext } from "../Context/useUserData"
 
 const MyArchive = () => {
     const { data } = UseDataContext();
-    const favList = data.favList;
+    const favList = data.favorite;
 
     return (
         <div className="favRecipeList">
             {
-                favList.length === 0
+                (favList.length === 0)
                 ? <h3 id="nothingText">Nothing here.<br/> Go find some nice recipes.</h3> 
                 : favList.map((e) => {return (
                     <p>{e}</p>
