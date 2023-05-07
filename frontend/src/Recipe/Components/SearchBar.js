22b15b3bf2b5cfde8c9b5f7d7e65fe03b539b5ab
zhiyuuuu <b09705038@ntu.edu.tw>
2023-05-07 15:23:09 +0800

    add searchbar ingredients api

--- frontend/src/Recipe/Components/SearchBar.js
+++ frontend/src/Recipe/Components/SearchBar.js
@@ -2,43 +2,110 @@ import { useState, useRef, useEffect } from 'react';
 import './SearchBar.css';
 import { IoIosArrowDown } from 'react-icons/io';
 import { IoFilterCircleOutline } from 'react-icons/io5';
+import { apiAllIngredient } from '../../axios/noToken';
+// import { apiQueryRecipeByName } from '../../axios/withToken';
+import Spinner from './Spinner';
+
+
+const EmptyResultDisplay = () => {
+    return(
+        <div>
+            <p>No related ingredients :(</p>
+        </div>
+    )
+}
 
 const SearchBar = () => {
 
     const [toggle, setToggle] = useState(false);
     const [filter, setFilter] = useState('Name');
     const [inputDropdown, setInputDropdown] = useState(false);
+    const [ingredients, setIngredients] = useState([]);
+    const [searchedIng, setSearchedIng] = useState([]);
+
+    const [loading, setLoading] = useState(false);
+    const [resultIsEmpty, setResultIsEmpty] = useState(false);
+
+    //const ingredients = [];
 
     const dropdownRefFilter = useRef(null);
     const dropdownRefInput = useRef(null);
 
-    const ingredients = [
+    // const paletteCategory2Color = {
+    //     "Vegetables": "#F6C478",
+    //     "Meat and Poultry": "#E9A894",
+    //     "Seafood": "#F0C9D6",
+    //     "Grains": "#AAC2B4",
+    //     "Fruits": "#C2BBE3",
+    //     "Dairy": "#B4C8E4",
+    //     "Nuts and Seeds": "#DDBFA5",
+    //     "notActive": "#DDDDDD"
+    // };
+
+    // const categoryRows = {
+    //     0: {id: 1, cateName: 'Vegetables'},
+    //     1: {id: 2, cateName: 'Dairy'},
+    //     2: {id: 3, cateName: 'Meat and Poultry'},
+    //     3: {id: 4, cateName: 'Seafood'},
+    //     4: {id: 5, cateName: 'Grains'}, 
+    //     5: {id: 6, cateName: 'Fruits'}, 
+    //     6: {id: 7, cateName: 'Nuts and Seeds'}, 
+    //     7: {id: 8, cateName: 'Soures'}
+    // }
+
+    const ingredientswColor = [
         {
-            name: 'Potato',
+            id: 1,
             category: 'Vegetables',
             color: "#F6C478"
         },
         {
-            name: 'Chicken',
+            id: 2,
+            category: 'Diary',
+            color: "#B4C8E4"
+        },
+        {
+            id: 3,
             category: 'Meat and Poultry',
             color: "#E9A894"
         },
         {
-            name: 'Beef',
-            category: 'Meat and Poultry',
-            color: "#E9A894"
+            id: 4,
+            category: 'Seafood',
+            color: "#F0C9D6"
         },
         {
-            name: 'Onion',
-            category: 'Vegetables',
-            color: "#F6C478"
+            id: 5,
+            category: 'Grains',
+            color: "#AAC2B4"
         },
         {
-            name: 'All ingredients in my fridge',
-            category: 'My fridge',
+            id: 6,
+            category: 'Fruits',
+            color: "#C2BBE3"
+        },
+        {
+            id: 7,
+            category: 'Nuts and Seeds',
+            color: "#DDBFA5"
+        },
+        {
+            id: 8,
+            category: 'Soures',
             color: "#C2BBE3"
         }
     ]
+
+    const handleIngSearch = async() => {
+        setLoading(true);
+        const allIngredients = await apiAllIngredient();
+        setLoading(false);
+        // ingredients.push(allIngredients.data.rows);
+        setIngredients(allIngredients.data.rows);
+        setSearchedIng(allIngredients.data.rows);
+        console.log('All ingredients', allIngredients);
+        // categoryColor();
+    }
     
     const handleSelect = (e) => {
         const select = e.target.innerHTML;
@@ -56,6 +123,7 @@ const SearchBar = () => {
         if (filter != 'Name') {
             setInputDropdown(!inputDropdown);
         }
+        handleIngSearch();
     }
 
     const handleFilterClickOutside = (event) => {
@@ -83,6 +151,29 @@ const SearchBar = () => {
         };
     }, []);
 
+    const handleSearch = async(filter, input) => {
+        console.log('search by', filter);
+        if (filter === 'Name'){
+            // const result = await apiQueryRecipeByName({ input })//非完整有包含嗎
+            // console.log('name search result', result);
+        } else {
+            let searchResult = []
+            ingredients.forEach((ingred) => {
+                if (ingred.ingredName.toLowerCase().includes(input)) {
+                    // console.log('searched ingredients: ', ingred.ingredName);
+                    searchResult.push(ingred)
+                }
+            })
+            if (searchResult.length === 0) {
+                setResultIsEmpty(true);
+            } else {
+                setResultIsEmpty(false);
+            }
+            setSearchedIng(searchResult);
+            // console.log('searched', searchedIng);
+        }
+    }
+
     return(
         <div className="searchbar-container">
             {/* <div className="search-introduction">
@@ -106,17 +197,22 @@ const SearchBar = () => {
                 <input type="text" 
                     id='search-bar' 
                     placeholder={'Search recipes by ' + filter} 
-                    onClick={handleInputDropdown}/>
+                    onClick={handleInputDropdown}
+                    onChange={(e) => {
+                        setSearchedIng([]);
+                        handleSearch(filter, e.target.value);}}/>
             </div>
             <div className="search-dropdown" style={ inputDropdown? {maxHeight: '50vh'}:{maxHeight: '0'}}>
                 <div className="search-ingredients" ref={dropdownRefInput}>
+                    {loading && <Spinner />}
+                    {resultIsEmpty && <EmptyResultDisplay />}
                     {
-                        ingredients.map((ingred, idx) => {
+                        searchedIng.map((ingred, idx) => {
                             return <button type="button"
                                 key={idx} 
                                 className="ingredient" 
-                                style={{ background: ingred.color }}>
-                                    {ingred.name}
+                                style={{ background: ingredientswColor.find(obj => obj.id === ingred.categoryID).color }}>
+                                    {ingred.ingredName}
                             </button>
                         })
                     }
