6de5a7f0f5324a0823bbd2c7f0dd36fd34ff4fd2
Chia-Yi Lin <r10922124@ntu.edu.tw>
2023-05-08 00:08:28 +0800

    some changes at backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -43,10 +43,10 @@ db.sequelize.sync().then(() => {    //drop table if exists
 })
 
 
-app.use(express.static(path.join(__dirname, "..", "..", "UI", "build")));
-app.get("/*", (_, res) => {
-  res.sendFile(path.join(__dirname,"..","..", "UI", "build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "..", "..", "UI", "build")));
+// app.get("/*", (_, res) => {
+//   res.sendFile(path.join(__dirname,"..","..", "UI", "build", "index.html"));
+// });
 
 app.listen(PORT, function(err){
   if (err) console.log("Error in server setup")
