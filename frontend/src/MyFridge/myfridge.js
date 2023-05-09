1a17c35628a6fc387c5e29803ba4375e1ff6548c
Chia-Yi Lin <r10922124@ntu.edu.tw>
2023-05-10 00:16:34 +0800

    fix purchase/expire misuse

--- frontend/src/MyFridge/myfridge.js
+++ frontend/src/MyFridge/myfridge.js
@@ -525,10 +525,10 @@ const MyFridge = () => {
                     purchaseDateValid: true,
                     expirationDateValid: true,
                 })).sort((a, b) => new Date(a.purchaseDate) - new Date(b.purchaseDate));
-                const earliestExpirationDate = Math.min(...origFridgeData[key].map((item) => new Date(item.expire_date)));
-                const purchaseDate = new Date(earliestExpirationDate).toISOString().substring(0, 10);
                 const earliestPurchasenDate = Math.min(...origFridgeData[key].map((item) => new Date(item.purchase_date)));
-                const expirationDate = new Date(earliestPurchasenDate).toISOString().substring(0, 10);
+                const purchaseDate = new Date(earliestPurchasenDate).toISOString().substring(0, 10);
+                const earliestExpirationDate = Math.min(...origFridgeData[key].map((item) => new Date(item.expire_date)));
+                const expirationDate = new Date(earliestExpirationDate).toISOString().substring(0, 10);
 
                 transformedFridgeData.push({
                     pseudoId,
