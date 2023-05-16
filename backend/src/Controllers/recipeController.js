06d887cc3df9f1161af1807bbb308a0b6a2e1a41
wayneliu <72805367+wayne05IM@users.noreply.github.com>
2023-05-16 17:26:26 +0800

    recipe add query by ingredients and some bug fixing

--- backend/src/Controllers/recipeController.js
+++ backend/src/Controllers/recipeController.js
@@ -1,13 +1,14 @@
 import { pool } from "../Clients/pool";
 import dotenv from "dotenv-defaults";
 import { uploads } from "../Config/cloudinary";
-import fs from 'fs'
+import fs from "fs";
 dotenv.config();
 
 // recipe query
 const qeuryByID = async (req, res) => {
-  const {id} = req.query;
-  const query = 'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "userID" = "Users".id and "Recipes".id = $1';
+  const { id } = req.query;
+  const query =
+    'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "userID" = "Users".id and "Recipes".id = $1';
   const values = [parseInt(id)];
 
   try {
@@ -20,8 +21,9 @@ const qeuryByID = async (req, res) => {
 };
 
 const qeuryByName = async (req, res) => {
-  const {title} = req.query;
-  const query = 'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "Recipes"."userID" = "Users".id and LOWER("Recipes"."title") LIKE LOWER($1)';
+  const { title } = req.query;
+  const query =
+    'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "Recipes"."userID" = "Users".id and LOWER("Recipes"."title") LIKE LOWER($1)';
   const values = [`%${title.toString()}%`];
 
   try {
@@ -34,8 +36,9 @@ const qeuryByName = async (req, res) => {
 };
 
 const queryByLabel = async (req, res) => {
-  const {label} = req.query;
-  const query = 'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "Recipes"."userID" = "Users".id and $1 = ANY("Recipes"."labels")';
+  const { label } = req.query;
+  const query =
+    'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "Recipes"."userID" = "Users".id and $1 = ANY("Recipes"."labels")';
   const values = [parseInt(label)];
 
   try {
@@ -48,8 +51,9 @@ const queryByLabel = async (req, res) => {
 };
 
 const queryTopLikeCount = async (req, res) => {
-  const {page} = req.query;
-  const query = 'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "Recipes"."userID" = "Users".id ORDER BY "likeCount" DESC OFFSET $1 ROWS FETCH NEXT 15 ROWS ONLY';
+  const { page } = req.query;
+  const query =
+    'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "Recipes"."userID" = "Users".id ORDER BY "likeCount" DESC OFFSET $1 ROWS FETCH NEXT 15 ROWS ONLY';
   const values = [(parseInt(page) - 1) * 15];
 
   try {
@@ -62,9 +66,10 @@ const queryTopLikeCount = async (req, res) => {
 };
 
 const queryByIngredients = async (req, res) => {
-  const {ingredient} = req.query;
-  const query = 'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "Recipes"."userID" = "Users".id AND jsonb_exists(ingredients, $1)';
-  const values = [parseInt(ingredient)];
+  const { ingredient } = req.query;
+  const query =
+    'SELECT "Recipes"."id", title, overview, "Recipes"."servingSize", instructions, image, video, "Recipes"."likeCount", labels, ingredients, comments, "Recipes"."createdAt", "Recipes"."updatedAt", "userName" FROM "Recipes", "Users" WHERE "Recipes"."userID" = "Users".id AND EXISTS (SELECT 1 FROM json_each(ingredients) AS i WHERE i.key::int = ANY($1)) ORDER BY (SELECT COUNT(*) FROM json_object_keys(ingredients) AS keys WHERE keys::int = ANY($1)) DESC';
+  const values = [JSON.parse(ingredient)];
 
   try {
     const { rows } = await pool.query(query, values);
@@ -77,8 +82,10 @@ const queryByIngredients = async (req, res) => {
 
 // recipe update
 const updateAddLikeCount = async (req, res) => {
-  const {id} = req.body;
-  const query = 'UPDATE "Recipes" SET "likeCount" = "likeCount" + 1 WHERE id = $1';
+  const { id } = req.query;
+  console.log(id);
+  const query =
+    'UPDATE "Recipes" SET "likeCount" = "likeCount" + 1 WHERE id = $1';
   const values = [parseInt(id)];
 
   try {
@@ -91,8 +98,9 @@ const updateAddLikeCount = async (req, res) => {
 };
 
 const updateMinusLikeCount = async (req, res) => {
-  const {id} = req.body;
-  const query = 'UPDATE "Recipes" SET "likeCount" = "likeCount" - 1 WHERE id = $1';
+  const { id } = req.query;
+  const query =
+    'UPDATE "Recipes" SET "likeCount" = "likeCount" - 1 WHERE id = $1';
   const values = [parseInt(id)];
 
   try {
@@ -106,23 +114,29 @@ const updateMinusLikeCount = async (req, res) => {
 
 const updateRecipe = async (req, res) => {
   const {title, overview, servingSize, instructions, image, video, labels, ingredients, id} = req.body;
+  const user = req.user;
+  const file = req.file;
+  const { path } = file;
+  const newPath = await uploaderImage(path);
+  let url = newPath;
+  fs.unlinkSync(path);
   const query =
-    'UPDATE "Recipes" SET "title" = $1, "overview" = $2, "servingSize" = $3, "instructions" = $4, "image" = $5, "video" = $6, "labels" = $7, "ingredients" = $8 WHERE "id" = $9';
+    'UPDATE "Recipes" SET "title" = $1, "overview" = $2, "servingSize" = $3, "instructions" = $4, "image" = $5, "imagePID" = $6, "video" = $7, "labels" = $8, "ingredients" = $9 WHERE "id" = $10 and "userID" = $11';
   const values = [
     title.toString(),
     overview.toString(),
     parseInt(servingSize),
-    instructions,
-    image.toString(),
+    JSON.parse(instructions),
+    url.url,
+    url.id,
     video.toString(),
-    labels,
-    ingredients,
+    JSON.parse(labels),
+    JSON.parse(ingredients),
     parseInt(id),
+    user.id,
   ];
-
   try {
     await pool.query(query, values);
-
     res.send("success");
   } catch (err) {
     res.send("fail");
@@ -133,35 +147,31 @@ const updateRecipe = async (req, res) => {
 const getCommentUserData = async (req, res) => {
   const query = 'SELECT "photo", "userName" FROM "Users" WHERE "id" = $1';
   try {
-      const { id } = req.query;
-      const values = [
-        parseInt(id)
-      ];
-      const { rows } = await pool.query(query, values);
-      if (rows) {
-          res.status(200).send(rows);
-      } else {
-          res.status(400).send({message: 'Error ID'});
-      }
-      
-  }catch (err) {
-      console.log('getImage error');
-      console.log(err);
+    const { id } = req.query;
+    const values = [parseInt(id)];
+    const { rows } = await pool.query(query, values);
+    if (rows) {
+      res.status(200).send(rows);
+    } else {
+      res.status(400).send({ message: "Error ID" });
+    }
+  } catch (err) {
+    console.log("getImage error");
+    console.log(err);
   }
-}
+};
 
 const addComment = async (req, res) => {
-  const {comment, Rid} = req.body;
+  const { comment, Rid } = req.query;
   const query =
-    'UPDATE "Recipes" SET "comments" = $1 WHERE "id" = $2';
+    'UPDATE "Recipes" SET comments = comments || json_build_array($1::json) WHERE id = $2;';
   const values = [
-    comment,
-    parseInt(Rid)
+    JSON.parse(comment),
+    parseInt(Rid),
   ];
 
   try {
     await pool.query(query, values);
-
     res.send("success");
   } catch (err) {
     res.send("fail");
@@ -169,23 +179,25 @@ const addComment = async (req, res) => {
   }
 };
 
-const uploaderImage = async(file) => await uploads(file, 'Recipe');
+const uploaderImage = async (file) => await uploads(file, "Recipe");
 
 // add recipe
 const addRecipe = async (req, res) => {
-  console.log(req.body)
-  const {title, overview, servingSize, instructions, video, labels, ingredients} = req.body;
+  const {
+    title,
+    overview,
+    servingSize,
+    instructions,
+    video,
+    labels,
+    ingredients,
+  } = req.body;
   const user = req.user;
   const file = req.file;
-  let url = ''
   const { path } = file;
-  const newPath = await uploaderImage(path)
-  url = newPath
-  // console.log("url",url)
-  fs.unlinkSync(path)
-  // console.log("instructions",instructions)
-  // console.log(JSON.stringify(instructions))
-  // console.log(JSON.parse(instructions))
+  const newPath = await uploaderImage(path);
+  let url = newPath;
+  fs.unlinkSync(path);
   const query =
     'INSERT INTO "Recipes" ("userID", "title", "overview", "servingSize", "instructions", "image", "imagePID", "video", "likeCount", "labels", "ingredients", "comments") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, DEFAULT)';
   const values = [
@@ -201,13 +213,39 @@ const addRecipe = async (req, res) => {
     JSON.parse(labels),
     JSON.parse(ingredients),
   ];
-  console.log(values)
+  console.log(values);
   try {
     await pool.query(query, values);
     res.send("success");
   } catch (err) {
     res.send("fail");
-    console.log('add recipe failed')
+    console.log("add recipe failed");
+    console.log(err);
+  }
+};
+
+const deleteByID = async (req, res) => {
+  const { id } = req.query;
+  const query = 'DELETE FROM "Recipes" WHERE and "id" = $1';
+  const values = [parseInt(id)];
+
+  try {
+    await pool.query(query, values);
+    res.send("success");
+  } catch (err) {
+    res.send("fail");
+    console.log(err);
+  }
+};
+
+const queryAll = async (req, res) => {
+  const query = 'SELECT * FROM "public"."Recipes" LIMIT 100';
+
+  try {
+    const result = await pool.query(query);
+    res.send(result);
+  } catch (err) {
+    res.send("fail");
     console.log(err);
   }
 };
@@ -223,5 +261,7 @@ export {
   updateRecipe,
   addComment,
   addRecipe,
-  getCommentUserData
+  getCommentUserData,
+  deleteByID,
+  queryAll,
 };
