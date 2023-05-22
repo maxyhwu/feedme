d7096a6e612f8d0773ca558643fabc44f3c627b4
Chia-Yi Lin <r10922124@ntu.edu.tw>
2023-05-22 18:42:51 +0800

    update archived recipes after refreshing the page

--- frontend/src/MyArchive/MyArchive.js
+++ frontend/src/MyArchive/MyArchive.js
@@ -1,5 +1,5 @@
 import './MyArchive.css';
-
+import { useState, useEffect } from "react";
 import { UseDataContext } from "../Context/useUserData"
 import { RecipeObject } from '../Recipe/recipe';
 import { apiQueryRecipeByID } from '../axios/withToken'
@@ -7,23 +7,23 @@ import { recipe_data } from "../Recipe/recipedata";
 
 const MyArchive = () => {
     const { data } = UseDataContext();
-    // const favList = data.like;
-
-    // ad-hoc (need to be solved)
-    const favList = Array.from(new Set(data.like));
-
+    const [favList, setFavList] = useState(null);
+    useEffect(() => {
+        setFavList(data.like);
+    }, []);
+    
     return (
         <div className="favRecipeList">
             {
                 (favList == null || favList.length === 0)
                 ? <h3 id="nothingText">Nothing here.<br/> Go find some mouthwatering recipes.</h3> 
                 : <div className="savedRecipes-container">
-                    {favList.map((recipeID) => {
+                    {favList.map((recipeID, index) => {
                         // const recipeAPI = apiQueryRecipeByID(recipeID).then((recipe) => {return recipe});
                         const recipe = recipe_data[recipeID];
                         return (
                             <RecipeObject
-                                recipe={recipe}
+                                key={index} recipe={recipe}
                             />
                         )
                     })}
