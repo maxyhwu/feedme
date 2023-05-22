9744a321c8ce7ebf17e8b2de806145474ad8f342
Chia-Yi Lin <r10922124@ntu.edu.tw>
2023-05-22 18:42:07 +0800

    fix click heart bug, allow api data

--- frontend/src/HomePage/HomePage.js
+++ frontend/src/HomePage/HomePage.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./HomePage.css"
 import { FaHeart } from 'react-icons/fa';
 import { IoIosArrowForward } from 'react-icons/io'
 import { useNavigate } from "react-router-dom";
 import Modal from 'react-modal';
+import { RecipeObject } from '../Recipe/recipe';
 import { recipe_data } from "../Recipe/recipedata";
 import { RecipeDetail } from '../Recipe/detail';
+import { UseGeneralContext } from '../Context/generalTables'
+import { apiQueryRecipeByTop } from '../axios/withToken'
 
 const HomePage = () => {
 
@@ -19,8 +22,37 @@ const HomePage = () => {
         navigate('/modal');
     }
 
+    const { id2ingredient } = UseGeneralContext();
     const [activeHeart, setActiveHeart] = useState(false);
     const [activeHeart1, setActiveHeart1] = useState(false);
+    const [apiRecipeData, setApiRecipeData] = useState([]);
+
+    useEffect(() => {
+        const getRecipe = (page) => {
+            const promise = apiQueryRecipeByTop(page);
+            promise.then((value) => {
+                // console.log(value.data.rows);
+                setApiRecipeData((prevData) => {
+                    const newData = [...prevData];
+                    const startIndex = (page - 1) * 15;
+                    for (let i = 0; i < value.data.rows.length; i++) {
+                        const { id, title, overview, servingSize, instructions, image, video, likeCount, labels, ingredients, comments, createdAt, updatedAt, userName } = value.data.rows[i];
+                        const formatIngredients = Object.entries(ingredients).map(([id, amount]) => [id2ingredient[id], amount]);  // ...amount => ! amount is not iterable
+                        newData[startIndex + i] = {
+                            recipeID: id,
+                            recipeName: title,
+                            serving: servingSize,
+                            ingredients: formatIngredients,
+                            instructions: instructions,
+                            image_link: image,
+                        }
+                    }
+                    return newData;
+                });
+            })
+        }
+        getRecipe(1);
+    }, [])
 
     const toggle = () => {
         setActiveHeart(!activeHeart);
@@ -153,7 +185,7 @@ const HomePage = () => {
                     </div>
                     <button onClick={scrollDown}> Discover more </button>
                 </div>
-                <div className="middle" id="middlePart">
+                {/* <div className="middle" id="middlePart">
                     <div className="section-title" id="recommend-recipe">
                         Recommended Recipes
                     </div>
@@ -161,89 +193,15 @@ const HomePage = () => {
                         {recommendData.map((recipe, index) => (
                             <RecommendRecipeBlock key={index} recipe={ recipe } />
                         ))}
-
-                        {/* <div className="recipe">
-                            <div className="recipe-header">
-                                <div className="recipe-title"> Curry Rice </div>
-                                <FaHeart style={{ color: activeHeart ? 'red' : '' }} onClick={toggle}/>
-                            </div>
-                            <div className="view-block">
-                                
-                                <div className="recipe-content">
-                                    Ingredients:<br/><br/>
-
-                                    2 cups Japanese rice<br/>
-                                    4 cups water<br/>
-                                    1 large onion, chopped<br/>
-                                    2 carrots, chopped<br/>
-                                    2 potatoes, chopped<br/>
-                                    1 lb. chicken or beef, cut into bite-sized pieces<br/>
-                                    2 tbsp. vegetable oil<br/>
-                                    1 box (8.4 oz.) Japanese curry roux<br/>
-                                    Salt and pepper to taste<br/><br/>
-
-                                    Instructions:<br/>
-                                    <ol className="instructions">
-                                        <li>Rinse the rice in a strainer and let it soak in water for 30 minutes.</li>
-                                        <li>In a large pot, heat the vegetable oil over medium heat. Add the chopped onions and cook until they are translucent.</li>
-                                        <li>Add the chicken or beef to the pot and cook until it's no longer pink.</li>
-                                        <li>Add the chopped carrots and potatoes to the pot and cook for a few minutes until they start to soften.</li>
-                                        <li>Add 4 cups of water to the pot and bring it to a boil.</li>
-                                        <li>Turn the heat down to low and simmer for about 20 minutes until the vegetables are fully cooked.</li>
-                                        <li>Add the curry roux to the pot and stir until it dissolves. Simmer for another 10 minutes until the curry thickens.</li>
-                                        <li>Season with salt and pepper to taste.</li>
-                                        <li>To make the rice, drain the water from the rice and put it in a rice cooker with 2 cups of water. Cook according to the manufacturer's instructions.</li>
-                                        <li>Serve the curry over the rice and enjoy!</li>
-                                    </ol>
-                                </div>
-                            </div>
-                            
-                            <div className="learnmore">
-                                <button className="button-learnmore" onClick={navigateToDetail}>Learn More</button>
-                            </div>
-                        </div>
-                        <div className="recipe">
-                            <div className="recipe-header">
-                                    <div className="recipe-title"> Scrambled Egg </div>
-                                    <FaHeart style={{ color: activeHeart1 ? 'red' : '' }} onClick={toggle1}/>
-                                </div>
-                            <div className="view-block">
-            
-                                <div className="recipe-content">
-                                    Ingredients:<br/><br/>
-
-                                    2-3 medium-sized tomatoes, chopped<br/>
-                                    3-4 eggs<br/>
-                                    2 cloves garlic, minced<br/>
-                                    1/2 tsp cumin powder<br/>
-                                    1/2 tsp red chili flakes<br/>
-                                    Salt and black pepper to taste<br/>
-                                    2-3 tbsp oil<br/><br/>
-
-                                    Instructions:<br/>
-                                    <ol className="instructions">
-                                        <li>Heat oil in a non-stick frying pan over medium heat.</li>
-                                        <li>Add the minced garlic and sauté for a minute or until fragrant.</li>
-                                        <li>Add the chopped tomatoes, cumin powder, red chili flakes, salt, and black pepper. Stir everything together and cook for 5-7 minutes or until the tomatoes have softened and reduced into a thick sauce.</li>
-                                        <li>Crack the eggs directly into the tomato sauce, making sure to space them out evenly.</li>
-                                        <li>Cover the pan with a lid and cook for another 5-7 minutes, or until the eggs are cooked to your desired doneness.</li>
-                                        <li>Serve hot with your favorite bread or as a side dish to your main meal.</li>
-                                    </ol>
-                                </div>
-                            </div>
-                            <div className="learnmore">
-                                <button className="button-learnmore" onClick={navigateToDetail}>Learn More</button>
-                            </div>
-                        </div> */}
                     </div>
-                </div>
+                </div> */}
                 <div className="bottom">
                     <div className="section-title">
                         Our Popular Recipes
                     </div>
                     <div className="popRecipes-container">
                         {popularData.map((recipe, index) => (
-                            <PopularRecipeBlock key={index} recipe={ recipe } />
+                            <RecipeObject key={index} recipe={ recipe } />
                         ))}
                         {/* <div className="popRecipe" onClick={navigateToDetail}>
                             <div className="popImg">
