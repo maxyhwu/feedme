9b77ec7005cb4ed00083099835cdac38f25759bb
AnthonyWu1121 <91513821+AnthonyWu1121@users.noreply.github.com>
2023-05-30 12:27:59 +0800

    port 7184

--- frontend/src/Context/commentSocketHooks.js
+++ frontend/src/Context/commentSocketHooks.js
@@ -7,7 +7,7 @@ import { io } from 'socket.io-client';
 // dotenv.config();
 // console.log("dotenv = ", process.env.PORT)
 // const PORT = process.env.PORT || 8000
-const PORT = 7096
+const PORT = 7184
 
 // Connect to the Socket.IO server
 // const socket = io('http://localhost:3001'); // Replace with your server's URL
