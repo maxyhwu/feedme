c29573cbb6c57ca995d5a8fc8a64dd5d5050db5e
zhiyuuuu <b09705038@ntu.edu.tw>
2023-05-30 22:51:00 +0800

    fix searchbar bug

--- frontend/src/Recipe/Components/SearchBar.js
+++ frontend/src/Recipe/Components/SearchBar.js
@@ -153,24 +153,12 @@ const SearchBar = ({ setRecipe, setSearching }) => {
         }
     }
 
-    const handleInputClickOutside = (event) => {
-        if (dropdownRefInput.current && !dropdownRefInput.current.contains(event.target)) {
-            //if dropdown is opened and do not click on dropdown content
-            // console.log('event target', event.target);
-            
-            // if (choseIngredId.length === 0) {
-                setInputDropdown(false);
-            // } 
-            setSearching(false);
-        }
-    }
-
     useEffect(() => {
         document.addEventListener('mousedown', handleFilterClickOutside);
-        document.addEventListener('mousedown', handleInputClickOutside);
+        // document.addEventListener('mousedown', handleInputClickOutside);
         return () => {
           document.removeEventListener('mousedown', handleFilterClickOutside);
-          document.removeEventListener('mousedown', handleInputClickOutside);
+        //   document.removeEventListener('mousedown', handleInputClickOutside);
         };
     }, []);
 
@@ -234,6 +222,9 @@ const SearchBar = ({ setRecipe, setSearching }) => {
     const handleClearInput = () => {
         setInput('');
         setSearching(false);
+        setChoseIngred([]);
+        setChoseIngredId([]);
+        setInputDropdown(false);
     }
 
     const clickIngredToSearch = async(ingredient, type) => {
@@ -316,9 +307,10 @@ const SearchBar = ({ setRecipe, setSearching }) => {
                         onChange={(e) => {
                             setSearchedIng([]);
                             handleSearch(filter, e.target.value, e);}}/>
-                    {
+                    {/* {
                         input.length != 0 && <IoClose onClick={handleClearInput}/>
-                    }
+                    } */}
+                    <IoClose onClick={handleClearInput}/>
                 </div>
                 
             </div>
